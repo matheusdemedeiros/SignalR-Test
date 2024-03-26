44f72d56ceb10974bb21ba81dd52eb74dedc15a4
Matheus de Medeiros <matheus.medeiros@ndd.tech>
2024-03-26 13:08:28 -0300

    feat: adiciona o client .NET

---
+++ Clients/.NetClient/SignalClient/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Microsoft.AspNetCore.SignalR.Client;
+
+var uri = "http://localhost:5108/chat";
+
+
+var builder = new HubConnectionBuilder()
+    .WithUrl(uri)
+    .Build();
+
+builder.On<string>("ReceiveMessage", (string message) => Console.WriteLine(message));
+
+await builder.StartAsync();
+
+
+
+
+// void ReceiveMessage(string message){
+//     Console.WriteLine( message);
+// }
+
+    Console.WriteLine( "Esperando mensagem");
+while(true){
+
+}
+
+  // connection.On("ReceiveMessage", (string data) =>
+  // {
+  //   Console.WriteLine(data);
+  // });
+
+
+Console.WriteLine("Execução finalizada!");
